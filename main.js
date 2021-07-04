130f54d2a57a22a85940dd2319432f726a3d636e
Graeme <graemster@gmail.com>
2021-07-04 18:46:03 -0500

    test blockchain creation

--- main.js
+++ main.js
@@ -1,35 +1,41 @@
 const SHA256 = require('crypto-js/sha256');
 
 class Block {
-  constructor(index, timestamp, data, previousHash = ''){
-    this.index = index;
-    this.timestamp = timestamp;
-    this.data = data;
-    this.previousHash = previousHash;
-    this.hash = this.calculateHash();
-  }
+	constructor(index, timestamp, data, previousHash = '') {
+		this.index = index;
+		this.timestamp = timestamp;
+		this.data = data;
+		this.previousHash = previousHash;
+		this.hash = this.calculateHash();
+	}
 
-  calculateHash(){
-    return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
-  }
+	calculateHash() {
+		return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+	}
 }
 
-class Blockchain{
-  constructor(){
-    this.chain = [this.createGenesisBlock()];
-  }
+class Blockchain {
+	constructor() {
+		this.chain = [this.createGenesisBlock()];
+	}
 
-  createGenesisBlock(){
-    return new Block(0, "07/04/2021", "Genesis block", "0");
-  }
+	createGenesisBlock() {
+		return new Block(0, '04/07/2021', 'Genesis block', '0');
+	}
 
-  getLatestBlock(){
-    return this.chain[this.chain.length - 1];
-  }
+	getLatestBlock() {
+		return this.chain[this.chain.length - 1];
+	}
 
-  addBlock(newBlock){
-    newBlock.previousHash = this.getLatestBlock().hash;
-    newBlock.hash = newBlock.calculateHash;
-    this.chain.push(newBlock);
-  }
+	addBlock(newBlock) {
+		newBlock.previousHash = this.getLatestBlock().hash;
+		newBlock.hash = newBlock.calculateHash();
+		this.chain.push(newBlock);
+	}
 }
+
+let graemeCoin = new Blockchain();
+graemeCoin.addBlock(new Block(1, '01/07/2021', { amount: 4 }));
+graemeCoin.addBlock(new Block(2, '03/07/2021', { amount: 10 }));
+
+console.log(JSON.stringify(graemeCoin, null, 4));
